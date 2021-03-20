60d41f3cd2e58e17187c66628aa8e562284f93f7
Jesse <jesse@oDELLbeckham2.0>
2021-03-19 21:48:45 -0700

    plots

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pandas as pd
 from chembl_webresource_client.new_client import new_client
 
+import sys
+sys.path.append('/usr/local/lib/python.3.7/site-packages/')
+
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Descriptors, Lipinski
+
+import seaborn as sns
+sns.set(style='ticks')
+import matplotlib.pyplot as plt
+
 #Target search for Coronavirus
 target = new_client.target
 target_query = target.search('coronavirus')
@@ -28,7 +39,7 @@ for i in df.standard_value:
     else:
         bioactivity_class.append('intermediate')
 
-# print(bioactivity_class)
+# print(df)
 
 # mol_cid = []
 # for i in df.molecule_chembl_id:
@@ -50,6 +61,202 @@ df2=df[selection]
 bioactivity_class = pd.Series(bioactivity_class, name='bioactivity_class')
 df3 = pd.concat([df2, bioactivity_class], axis=1)
 
-print(df3)
+# print(df3.bioactivity_class)
 
 df3.to_csv('bioactivity_data_preprocessed.csv', index=False)
+# print(Descriptors)
+
+def lipinski(smiles, verbose=False):
+    moldata=[]
+    for elem in smiles:
+        mol=Chem.MolFromSmiles(elem)
+        moldata.append(mol)
+    
+    baseData=np.arange(1,1)
+    i=0
+    for mol in moldata:
+        desc_MolWt = Descriptors.MolWt(mol)
+        desc_MolLogP = Descriptors.MolLogP(mol)
+        desc_NumHDonors = Lipinski.NumHDonors(mol)
+        desc_NumHAcceptors = Lipinski.NumHAcceptors(mol)
+           
+        row = np.array([desc_MolWt,
+                        desc_MolLogP,
+                        desc_NumHDonors,
+                        desc_NumHAcceptors])   
+    
+        if(i==0):
+            baseData=row
+        else:
+            baseData=np.vstack([baseData, row])
+        i=i+1      
+    
+    columnNames=["MW","LogP","NumHDonors","NumHAcceptors"]   
+    descriptors = pd.DataFrame(data=baseData,columns=columnNames)
+    
+    return descriptors
+
+df_lipinski=lipinski(df3.canonical_smiles)
+# print(df_lipinski)
+
+df_combined = pd.concat([df3, df_lipinski], axis=1)
+# print(df_combined)
+
+def pIC50(input):
+    pIC50 = []
+
+    for i in input['standard_value_norm']:
+        molar = float(i)*(10**-9) # Converts nM to M
+        pIC50.append(-np.log10(molar))
+
+    input['pIC50'] = pIC50
+    x = input.drop('standard_value_norm', 1)
+        
+    return x
+
+# print(df_combined.standard_value.describe())
+
+def norm_value(input):
+    norm = []
+
+    for i in input['standard_value']:
+        if float(i) > 100000000:
+          i = 100000000
+        norm.append(i)
+
+    input['standard_value_norm'] = norm
+    x = input.drop('standard_value', 1)
+        
+    return x
+
+df_norm = norm_value(df_combined)
+
+# print(df_norm.standard_value_norm.describe())
+
+df_final = pIC50(df_norm)
+# print(df_final.pIC50.describe())
+# print(df_final.head)
+
+df_2class = df_final[df_final.bioactivity_class != 'intermediate']
+print(df_2class)
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.countplot(x='bioactivity_class', data=df_2class, edgecolor='black')
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('Frequency', fontsize=14, fontweight='bold')
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.scatterplot(x='MW', y='LogP', data=df_2class, hue='bioactivity_class', size='pIC50', edgecolor='black', alpha=0.7)
+
+plt.xlabel('MW', fontsize=14, fontweight='bold')
+plt.ylabel('LogP', fontsize=14, fontweight='bold')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
+plt.savefig('plot_MW_vs_LogP.pdf')
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.boxplot(x = 'bioactivity_class', y = 'pIC50', data = df_2class)
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('pIC50 value', fontsize=14, fontweight='bold')
+
+plt.savefig('plot_ic50.pdf')
+
+def mannwhitney(descriptor, verbose=False):
+  # https://machinelearningmastery.com/nonparametric-statistical-significance-tests-in-python/
+  from numpy.random import seed
+  from numpy.random import randn
+  from scipy.stats import mannwhitneyu
+
+# seed the random number generator
+  seed(1)
+
+# actives and inactives
+  selection = [descriptor, 'bioactivity_class']
+  df = df_2class[selection]
+  active = df[df.bioactivity_class == 'active']
+  active = active[descriptor]
+
+  selection = [descriptor, 'bioactivity_class']
+  df = df_2class[selection]
+  inactive = df[df.bioactivity_class == 'inactive']
+  inactive = inactive[descriptor]
+
+# compare samples
+  stat, p = mannwhitneyu(active, inactive)
+  #print('Statistics=%.3f, p=%.3f' % (stat, p))
+
+  # interpret
+  alpha = 0.05
+  if p > alpha:
+    interpretation = 'Same distribution (fail to reject H0)'
+  else:
+    interpretation = 'Different distribution (reject H0)'
+  
+  results = pd.DataFrame({'Descriptor':descriptor,
+                          'Statistics':stat,
+                          'p':p,
+                          'alpha':alpha,
+                          'Interpretation':interpretation}, index=[0])
+  filename = 'mannwhitneyu_' + descriptor + '.csv'
+  results.to_csv(filename)
+
+  return results
+
+print(mannwhitney('pIC50'))
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.boxplot(x = 'bioactivity_class', y = 'MW', data = df_2class)
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('MW', fontsize=14, fontweight='bold')
+
+plt.savefig('plot_MW.pdf')
+
+print(mannwhitney('MW'))
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.boxplot(x = 'bioactivity_class', y = 'LogP', data = df_2class)
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('LogP', fontsize=14, fontweight='bold')
+
+plt.savefig('plot_LogP.pdf')
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.boxplot(x = 'bioactivity_class', y = 'LogP', data = df_2class)
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('LogP', fontsize=14, fontweight='bold')
+
+plt.savefig('plot_LogP.pdf')
+
+print(mannwhitney('LogP'))
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.boxplot(x = 'bioactivity_class', y = 'NumHDonors', data = df_2class)
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('NumHDonors', fontsize=14, fontweight='bold')
+
+plt.savefig('plot_NumHDonors.pdf')
+
+print(mannwhitney('NumHDonors'))
+
+plt.figure(figsize=(5.5, 5.5))
+
+sns.boxplot(x = 'bioactivity_class', y = 'NumHAcceptors', data = df_2class)
+
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('NumHAcceptors', fontsize=14, fontweight='bold')
+
+plt.savefig('plot_NumHAcceptors.pdf')
+
+print(mannwhitney('NumHAcceptors'))
